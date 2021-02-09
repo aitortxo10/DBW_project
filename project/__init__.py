6535ee43ca6f5d8bdb679625270da83e2c5eb3e9
aitortxo10 <gonzalezherreroaitor@gmail.com>
2021-02-09 12:47:39 +0100

    login and logout deployed

--- project/__init__.py
+++ project/__init__.py
@@ -1,18 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
+import os
 # init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
+
+flask_key=os.urandom(16)
 
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
+    #We need to set ut a key for the flask login function
+    app.config['SECRET_KEY'] = flask_key
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
     db.init_app(app)
 
+    #Flask has a built in login manager which we will use to keep track of the user session. Before initializing the app, we need to set up how it
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
@@ -21,7 +25,7 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(int(user_id))
+        return User.query.get(int(user_id)) #we will use the primary key of the user table to identify the user in the session
 
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
--- project/auth.py
+++ project/auth.py
@@ -10,13 +10,30 @@ auth = Blueprint('auth', __name__)
 def login():
     return render_template('login.html')
 
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember=True if request.form.get('remember') else False
+
+    user=User.query.filter_by(email=email).first()#Load the information related to the user
+
+    if not user or not check_password_hash(user.password,password): #If the password or the email is not correct, flash an error message and redirectonce more to the login page
+        flash('Incorrect user or password, please try again')
+        return redirect(url_for('auth.login'))
+
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile')) #If the login info is correct, load the profile of the user
+
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('main.index'))
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():                                  #First extract all the data from the signup form
