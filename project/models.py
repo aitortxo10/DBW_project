15b6bc1c896426eae325edd627ee61b4ffc3f72d
aitortxo10 <gonzalezherreroaitor@gmail.com>
2021-02-26 17:48:26 +0100

    Basic css + models added

--- project/models.py
+++ project/models.py
@@ -6,3 +6,20 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+class Challenge(db.Model):
+    idChallenge = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000))
+    Instructions = db.Column(db.Text(10000000))
+    Test_data = db.Column(db.Text(10000000))
+    Solution = db.Column(db.Text(10000000))
+    Hint = db.Column(db.Text(10000000))
+    Level = db.Column(db.Integer)
+
+class Category(db.Model):
+    idCategory = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000))
+
+class Programming_language(db.Model):
+    idLanguage = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
