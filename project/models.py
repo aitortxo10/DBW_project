47cf91af9dfc925b776fd96b1b91257d0bfe2e7a
arnaulb <arnaullinasb@gmail.com>
2021-03-06 10:56:20 +0100

    Updated DB schema (including sql script) and models.py. Created script to load the database with the challenges.

--- project/models.py
+++ project/models.py
@@ -1,25 +1,49 @@
 from flask_login import UserMixin
 from . import db
 
-class User(UserMixin, db.Model):
+# Many-to-Many relationships needs to a helper table (established wtih db.Table)
+categories_challenges = db.Table('categories_challenges',
+                                db.Column('categories_id', db.Integer, db.ForeignKey('categories.id'), primary_key=True),
+                                db.Column('challenges_id', db.Integer, db.ForeignKey('challenges.id'), primary_key=True))
+
+languages_challenges = db.Table('languages_challenges',
+                                db.Column('challenges_id', db.Integer, db.ForeignKey('challenges.id'), primary_key=True),
+                                db.Column('programming_languages_id', db.Integer, db.ForeignKey('programming_languages_id'), primary_key=True),
+                                db.Column('example_code', db.Text))
+
+challenges_stats = db.Table('challenges_stats',
+                            db.Column('challenges_id', db.Integer, db.ForeignKey('challenges_id'), primary_key=True),
+                            db.Column('users_id', db.Integer, db.ForeignKey('users_id'), primary_key=True),
+                            db.Column('start_date', db.DateTime),
+                            db.Column('end_date', db.DateTime),
+                            db.Column('tries', db.Integer),
+                            db.Column('solved', db.Boolean),
+                            db.Column('programming_languages_id', db.Integer, db.ForeignKey('programming_languages_id')))
+
+
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    password = db.Column(db.String(256))
     name = db.Column(db.String(1000))
+    age = db.Column(db.Integer)
+    country = db.Column(db.String(45))
+    background = db.Column(db.String(45))
 
-class Challenge(db.Model):
-    idChallenge = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(1000))
-    Instructions = db.Column(db.Text(10000000))
-    Test_data = db.Column(db.Text(10000000))
-    Solution = db.Column(db.Text(10000000))
-    Hint = db.Column(db.Text(10000000))
-    Level = db.Column(db.Integer)
 
-class Category(db.Model):
-    idCategory = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(1000))
+class Challenges(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    instructions = db.Column(db.Text)
+    test_data = db.Column(db.Text)
+    solution = db.Column(db.Text)
+    hint = db.Column(db.Text)
+    level = db.Column(db.Integer)
 
-class Programming_language(db.Model):
-    idLanguage = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
+class Categories(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(45))
+
+class ProgrammingLanguages(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(45))
