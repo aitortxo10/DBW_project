69599109f9ae9e44af48358a2620dd32aaa91f99
arnaulb <arnaullinasb@gmail.com>
2021-03-18 13:59:44 +0100

    Modified loadDB script to fill the languages_challenges table.

--- project/loadDB.py
+++ project/loadDB.py
@@ -4,8 +4,8 @@ import os
 # Connect to database
 database = "mydb"
 host = "localhost"
-user = "root"
-passwd = "password"
+user = "arnau"
+passwd = "Contrasenya"
 
 connection = pymysql.connect(host='localhost',
                 user=user,
@@ -20,6 +20,8 @@ connection.cursor().execute("SET FOREIGN_KEY_CHECKS=0")
 
 # loading challenges from the folder problems/
 sthProblems = "INSERT INTO challenges ({}) VALUES ({});"
+sthLP = "INSERT INTO languages_challenges VALUES ({});"
+
 for problem in os.listdir("problems"):
     with open("problems/"+problem, 'r') as fh:
 
@@ -28,32 +30,52 @@ for problem in os.listdir("problems"):
 
         # from each line extract the content and the column of the table where it belongs
         for line in fh:
-            line = line.rstrip()
-            column, value = line.split(" ; ")
 
-            # ignore empty lines
-            if not column or not value:
+            line = line.rstrip()
+            if not line: # ignore empty lines
                 continue
 
-            # construct a single string with all the column, values in the same order
-            # to use in the SQL command
-            columns += (column + ", ")
-            values += ("'" + value + "', ")
+            column, value = line.split(" ; ")
 
-        command = sthProblems.format(columns[:-2], values[:-2])
-        print(command)
-        with connection.cursor() as c:
-            c.execute(command)
-        print(problem)
+            if (column == "Languages"): # this will be done when reaching the last lines of the file if it is in the correct format
 
-# manually loading the programming languages
-sthLanguages = "INSERT INTO programming_languages ({}) VALUES ('{}');"
-languages = ["Python 3.8", "Perl 5", "R 4.0.2"]
-i = 0
-for language in languages:
-    command = sthLanguages.format("name, id", language + "', '" + str(i))
-    print(command)
-    with connection.cursor() as c:
-        c.execute(command)
-    print(language)
-    i += 1
+                # As we have explored all the file already, include that information into the Challenges table
+                command = sthProblems.format(columns[:-2], values[:-2]) # slicing to avoid last ","
+                print(command)
+                with connection.cursor() as c:
+                    c.execute(command)
+                    c_id = c.lastrowid # get challenge id
+
+                # get all the languages that the problem can be solved in
+                for lang in value.strip().split(","):
+                    print(lang)
+                    # check if language is in programming_languages table or not
+                    with connection.cursor() as c:
+                        com = "SELECT id FROM programming_languages WHERE name = {};".format("'" + lang + "'")
+                        print(com)
+                        c.execute(com)
+                        l_id = c.fetchone()[0]
+                        print(l_id)
+
+                        # if language not yet in the table
+                        if not l_id:
+                            to_exec = "INSERT INTO programming_languages (name) VALUES ({});".format("'" + lang + "'")
+                            print(to_exec)
+                            c.execute(to_exec)
+                            l_id = c.lastrowid
+
+
+                        # add the languages available into the languages_challenges table
+                        comm = sthLP.format("'" + str(c_id) + "'," +  "'" + str(l_id) + "'," + "'example_code'")
+                        print(comm)
+                        c.execute(comm)
+
+            elif (column == "Categories"):
+                # this will occur after loading the challenge in the table and filling the languages_challenges table
+                pass
+
+            else:
+                # construct a single string with all the column, values in the same order
+                # to use in the SQL command
+                columns += (column + ", ")
+                values += ("'" + value + "', ")
