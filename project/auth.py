55a09e894a306fb7abe8659d3537807cdd6b3013
aitortxo10 <gonzalezherreroaitor@gmail.com>
2021-03-29 12:52:09 +0200

    Boton de start challenge registra el tiempo

--- project/auth.py
+++ project/auth.py
@@ -5,7 +5,6 @@ from .models import *
 from . import db
 from email_validator import validate_email, EmailNotValidError
 import datetime
-from pylint.lint import Run
 
 auth = Blueprint('auth', __name__)
 
@@ -81,95 +80,37 @@ def exercises():
 @login_required
 def detailed_exercise(id, language):
     # use info from the url instead of a request form
-    user_id=current_user.id
     id=id
     language=language
     challenge=Challenges.query.filter_by(id=id).first()
-    Stats = ChallengesStats.query.filter_by(challenges_id=id,users_id=user_id).first()
-    return render_template('detailed_exercise.html',challenge=challenge, id=id, language=language, Stats=Stats)
+    return render_template('detailed_exercise.html',challenge=challenge, id=id, language=language)
 
-@auth.route('/detailed_exercise/<id>/<language>', methods=['POST'])
-def detailed_exercise_post(id, language):
+@auth.route('/test', methods=['POST'])
+def testing():
     # we have to get challenge id and programming language from URL
     user_id = current_user.id
-    id=id
-    language=language
 
-    correct_answer = Challenges.query.with_entities(Challenges.solution).filter_by(id=id).first()
+    correct_answer = Challenges.query.with_entities(Challenges.solution).filter_by(id=1).first()
     answer = request.form.get('answer')
     correct = (correct_answer == answer)
+    var = Users.query.with_entities(Users.name, Users.email).join(ChallengesStats).join(Challenges).first()
 
-    if correct == True:
-        var = Users.query.with_entities(Users.name, Users.email).join(ChallengesStats).join(Challenges).first()
+    ts = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-        time=datetime.datetime.now()
-        ts = time.strftime('%Y-%m-%d %H:%M:%S')
+    # check if user has tried the challenge already
+    var = ChallengesStats.query.filter_by(challenges_id=1,users_id=user_id).first()
 
-        # check if user has tried the challenge already
-        var = ChallengesStats.query.filter_by(challenges_id=id,users_id=user_id).first()
-
-        if var: # if it exists, update some of the stats
-            var.solved = correct
-            var.end_date=ts
-            var.tries += 1
-        else:
-            var = ChallengesStats(challenges_id=id,  users_id=user_id, start_date=ts, end_date =ts if correct else None, tries=1, programming_languages_id=1)
-
-        start = datetime.datetime.strptime(var.start_date, '%Y-%m-%d %H:%M:%S')
-        df=(time-start).total_seconds()
-        reference=df/172800  #Two days should be the standard to solve a problem
-
-        if reference <= 1:
-            pen_time=0
-        elif reference<=1.5:
-            pen_time=0.5
-        elif reference <=2:
-            pen_time=1
-        elif reference <=2.5:
-            pen_time=1.5
-        elif reference <= 3:
-            pen_time=2
-        else:
-            pen_time=2.5
-
-        if var.tries >4:
-            pen_tries = 2.5
-        else:
-            pen_tries = 0.5*var.tries
-
-        hints=request.form.get("hints")
-
-        uploaded_file=request.files['files']
-        if uploaded_file:
-            results = Run([uploaded_file], do_exit=False)
-            mark=results.linter.stats['global_note']
-
-            if mark <=0:
-                pen_pyl=2.5
-            elif mark <=2:
-                pen_pyl=2
-            elif mark <=4:
-                pen_pyl=1.5
-            elif mark <=6:
-                pen_pyl=1
-            elif mark <=8:
-                pen_pyl=0.5
-            else:
-                pen_pyl=0
-        else:
-            pen_pyl=2.5
-
-        score=10 - 2.5*hints - pen_tries - pen_time -pen_pyl
-
-        db.session.add(var)
-        db.session.commit()
-
-        flash("Problem solved correctly")
-        return redirect(url_for('exercises'))
-
+    if var: # if it exists, update some of the stats
+        var.solved = correct
+        var.end_date=ts if correct else None
+        var.tries += 1
     else:
-        flash("Incorrect Answer")
-        return render_template('detailed_exercise.html',challenge=challenge, id=id, language=language)
+        var = ChallengesStats(challenges_id=1,  users_id=user_id, start_date=ts, end_date =ts if correct else None, tries=1, programming_languages_id=1)
+        db.session.add(var)
+
+    db.session.commit()
+
+    return str(correct_answer == answer)
 
 # background process happening without any refreshing
 @auth.route('/background_process_test')
@@ -182,8 +123,7 @@ def background_process_test():
     var = ChallengesStats.query.filter_by(users_id=user_id, challenges_id=2, programming_languages_id=2).first()
 
     if var: # if the entry exists (meaning user already started/tried the challenge before)
-        if not var.start_date: # if start time was not set before, set it now
-            var.start_date = ts
+        var.start_date=ts
     else: # create the entry with the current timestamp as start_date
         var = ChallengesStats(challenges_id=2,  users_id=user_id, start_date=ts, end_date=None, programming_languages_id=2)
         db.session.add(var)
