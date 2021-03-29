45e835757b9910e489516569a08fab0dd6f0a4f4
aitortxo10 <gonzalezherreroaitor@gmail.com>
2021-03-29 13:19:53 +0200

    Se habia borrado lo de la evaluacion

--- project/auth.py
+++ project/auth.py
@@ -83,13 +83,17 @@ def detailed_exercise(id, language):
     id=id
     language=language
     challenge=Challenges.query.filter_by(id=id).first()
-    return render_template('detailed_exercise.html',challenge=challenge, id=id, language=language)
+    Stats = ChallengesStats.query.filter_by(challenges_id=id,users_id=user_id).first()
+    return render_template('detailed_exercise.html',challenge=challenge, id=id, language=language, Stats=Stats)
 
-@auth.route('/test', methods=['POST'])
-def testing():
+@auth.route('/detailed_exercise/<id>/<language>', methods=['POST'])
+def detailed_exercise_post(id, language):
     # we have to get challenge id and programming language from URL
     user_id = current_user.id
+    id=id
+    language=language
 
+    correct_answer = Challenges.query.with_entities(Challenges.solution).filter_by(id=id).first()
     correct_answer = Challenges.query.with_entities(Challenges.solution).filter_by(id=1).first()
     answer = request.form.get('answer')
     correct = (correct_answer == answer)
@@ -97,6 +101,67 @@ def testing():
 
     ts = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+    if correct == True:
+        var = Users.query.with_entities(Users.name, Users.email).join(ChallengesStats).join(Challenges).first()
+
+        time=datetime.datetime.now()
+        ts = time.strftime('%Y-%m-%d %H:%M:%S')
+
+        # check if user has tried the challenge already
+        var = ChallengesStats.query.filter_by(challenges_id=id,users_id=user_id).first()
+
+        if var: # if it exists, update some of the stats
+            var.solved = correct
+            var.end_date=ts
+            var.tries += 1
+        else:
+            var = ChallengesStats(challenges_id=id,  users_id=user_id, start_date=ts, end_date =ts if correct else None, tries=1, programming_languages_id=1)
+
+        start = datetime.datetime.strptime(var.start_date, '%Y-%m-%d %H:%M:%S')
+        df=(time-start).total_seconds()
+        reference=df/172800  #Two days should be the standard to solve a problem
+
+        if reference <= 1:
+            pen_time=0
+        elif reference<=1.5:
+            pen_time=0.5
+        elif reference <=2:
+            pen_time=1
+        elif reference <=2.5:
+            pen_time=1.5
+        elif reference <= 3:
+            pen_time=2
+        else:
+            pen_time=2.5
+
+        if var.tries >4:
+            pen_tries = 2.5
+        else:
+            pen_tries = 0.5*var.tries
+
+        hints=request.form.get("hints")
+
+        uploaded_file=request.files['files']
+        if uploaded_file:
+            results = Run([uploaded_file], do_exit=False)
+            mark=results.linter.stats['global_note']
+
+            if mark <=0:
+                pen_pyl=2.5
+            elif mark <=2:
+                pen_pyl=2
+            elif mark <=4:
+                pen_pyl=1.5
+            elif mark <=6:
+                pen_pyl=1
+            elif mark <=8:
+                pen_pyl=0.5
+            else:
+                pen_pyl=0
+        else:
+            pen_pyl=2.5
+
+        score=10 - 2.5*hints - pen_tries - pen_time -pen_pyl
     # check if user has tried the challenge already
     var = ChallengesStats.query.filter_by(challenges_id=1,users_id=user_id).first()
 
@@ -107,10 +172,17 @@ def testing():
     else:
         var = ChallengesStats(challenges_id=1,  users_id=user_id, start_date=ts, end_date =ts if correct else None, tries=1, programming_languages_id=1)
         db.session.add(var)
+        db.session.commit()
 
+        flash("Problem solved correctly")
+        return redirect(url_for('exercises'))
     db.session.commit()
 
+    else:
+        flash("Incorrect Answer")
+        return render_template('detailed_exercise.html',challenge=challenge, id=id, language=language)
     return str(correct_answer == answer)
+
 
 # background process happening without any refreshing
 @auth.route('/background_process_test')
